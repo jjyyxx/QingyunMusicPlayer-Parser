3d24377c8da2d37a0bcaf6d5c3fbbbabfd362e61
jjyyxx <obfishstudio@gmail.com>
2018-02-19 19:43:53 +0800

    error framework

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ const Parser = require('./Parser')
 const MMAAdapter = require('./adapter/MMAAdapter')
 const MIDIAdapter = require('./adapter/MIDIAdapter')
 
-Error.prototype.toJSON = function () {
-    return {
-        message: this.message
-    }
-}
-
 module.exports = {
     Parser,
     MMAAdapter,
